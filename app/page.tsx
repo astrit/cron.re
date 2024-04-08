11861a8ea6f5eaac89d2aea231dd1365a252e843
astrit <am@astrit.co>
2024-04-08 22:27:17 +0200

    Msg → Mon Apr  8 22:27:17 CEST 2024

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
       <h1>Calist.</h1>
+      <p>No BS* Calendar.</p>
+      <figure>
+        <Image
+          src="/raycast.png"
+          alt="Calist on Raycast"
+          width={2000}
+          height={1250}
+        />
+      </figure>
     </main>
   );
 }
