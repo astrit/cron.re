d894b7a461c90bbb78a183a76905708e69ea1fd6
astrit <am@astrit.co>
2024-04-26 13:13:39 +0200

    Msg → Fri Apr 26 13:13:39 CEST 2024

--- design/block/preloader/preloader.tsx
+++ design/block/preloader/preloader.tsx
@@ -1,5 +1,23 @@
-import "@/preloader/preloader.css";
+"use client"
+
+import { useContext, useEffect, useRef } from "react"
+import { Controller } from "$/provider/provider"
+
+import "@/preloader/preloader.css"
 
 export default function Preloader() {
-  return <div className="preloader">...</div>;
+  const { loading } = useContext(Controller)
+  const preloaderRef = useRef(null)
+
+  useEffect(() => {
+    if (!loading && preloaderRef.current) {
+      ;(preloaderRef.current as unknown as HTMLDivElement).style.opacity = "0"
+    }
+  }, [loading])
+
+  return (
+    <div className="preloader" ref={preloaderRef}>
+      <div className="load"></div>
+    </div>
+  )
 }
--- utils/provider/provider.tsx
+++ utils/provider/provider.tsx
@@ -1,49 +1,67 @@
-"use client";
+"use client"
 
-import React, { createContext, useEffect, useState } from "react";
-import { usePathname, useSearchParams } from "next/navigation";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-import { type ThemeProviderProps } from "next-themes/dist/types";
+import React, { createContext, useEffect, useState } from "react"
+import { usePathname, useSearchParams } from "next/navigation"
+import Cursor from "@/cursor/cursor"
+import Noise from "@/noise/noise"
+import Preloader from "@/preloader/preloader"
+import dev from "$/env/env"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
+import { Toaster } from "sonner"
 
 interface Data {
-  pages: { [x: string]: any }[];
+  pages: { [x: string]: any }[]
 }
 
 type Types = {
-  loading: boolean;
-  data: Data | null;
-};
+  loading: boolean
+  data: Data | null
+}
 
 export const Controller = createContext<Types>({
   loading: false,
   data: null,
-});
+})
 
 export function Provider({ children, ...props }: ThemeProviderProps) {
-  const [loading, setLoading] = useState(true);
-  const [data, setData] = useState<Data | null>(null);
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState<Data | null>(null)
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
 
   useEffect(() => {
-    setLoading(true);
+    setLoading(true)
     fetch("/api/data")
       .then((response) => response.json())
       .then((data) => {
         setTimeout(() => {
-          setData(data as Data);
-          setLoading(false);
-        }, 842);
-      });
-  }, [pathname, searchParams]);
+          setData(data as Data)
+          setLoading(false)
+        }, 842)
+      })
+  }, [pathname, searchParams])
 
   const value = {
     loading,
     data,
-  };
+  }
   return (
     <NextThemesProvider {...props}>
-      <Controller.Provider value={value}>{children}</Controller.Provider>
+      <Controller.Provider value={value}>
+        <Preloader />
+        {children}
+        <Toaster
+          richColors
+          theme="dark"
+          position="bottom-right"
+          expand={false}
+          closeButton={true}
+          duration={4428}
+        />
+        {!dev() && <Noise />}
+        <Cursor />
+      </Controller.Provider>
     </NextThemesProvider>
-  );
+  )
 }
